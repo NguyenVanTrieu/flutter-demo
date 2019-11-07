7266d1f8a714b11af4a0b5b104165fbbef74425a
trieunv <tenlagi16111@gmail.com>
2019-11-07 16:44:03 +0700

    Bỏ coment

--- lib/src/module/product/product_list_bloc.dart
+++ lib/src/module/product/product_list_bloc.dart
@@ -1,16 +1,10 @@
 import 'dart:async';
-import 'package:decimal/decimal.dart';
 import 'package:demo_flutter_app/src/base/base_bloc.dart';
 import 'package:demo_flutter_app/src/base/base_event.dart';
 import 'package:demo_flutter_app/src/data/repository/product_repository.dart';
-import 'package:demo_flutter_app/src/model/list_paged.dart';
 import 'package:demo_flutter_app/src/model/product.dart';
 
 class ProductListBloc extends BaseBloc {
-
-/*  StreamController<List<Product>> _streamControllerListProduct = StreamController<List<Product>>();
-  Stream<List<Product>> get listProductStream => _streamControllerListProduct.stream;
-  Sink<List<Product>> get _listProductSink => _streamControllerListProduct.sink;*/
 
   ProductRepository _productRepository = ProductRepository();
 
@@ -30,8 +24,6 @@ class ProductListBloc extends BaseBloc {
 
   @override
   void dispose() {
-//    _streamControllerListProduct.close();
-
     super.dispose();
   }
 }
