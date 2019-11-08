b237545c78dc199cb43daeb881dd2723a6b94f09
trieunv <tenlagi16111@gmail.com>
2019-11-08 15:22:55 +0700

    Lấy call danh sách - await

--- lib/src/module/product/product_list_bloc.dart
+++ lib/src/module/product/product_list_bloc.dart
@@ -2,24 +2,21 @@ import 'dart:async';
 import 'package:demo_flutter_app/src/base/base_bloc.dart';
 import 'package:demo_flutter_app/src/base/base_event.dart';
 import 'package:demo_flutter_app/src/data/repository/product_repository.dart';
+import 'package:demo_flutter_app/src/model/list_paged.dart';
 import 'package:demo_flutter_app/src/model/product.dart';
 
 class ProductListBloc extends BaseBloc {
 
   ProductRepository _productRepository = ProductRepository();
-
   @override
   void dispatchEvent(BaseEvent event) {
 
   }
 
   Future<List<Product>> pageData(int previousCount) async {
+    ListPaged<Product> products = await _productRepository.getProducts(null, (previousCount / 14).floor(), 14);
 
-    List<Product> products;
-    await _productRepository.getProducts(null, (previousCount / 14).floor(), 14).then((value){
-      products = value.items;
-    });
-    return products;
+    return products.items;
   }
 
   @override
